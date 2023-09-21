301ccf89e855c92771f648e1b311f37f303b407c
Munaimun <munaimunz@gmail.com>
2023-09-22 00:35:56 +0600

    installed tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
     <>
       <div>
-        <h1>testing</h1>
+        <h1 className='font-extrabold text-4xl underline'>InteliTalk</h1>
+        <div id="box" className='w-4/5'>
+          <p className='note'>Welcome to the University Chatbot! How can I assist you today? Whether you have questions about courses, admission, campus facilities, or anything else related to the university, feel free to ask, and I'll provide you with the information you need.</p>
+        </div>
       </div>
     </>
   )
