579a8cecc03c7fcb4ea40df6726548ca4ff79537
dev-saiful <saifulcseian@gmail.com>
2023-10-24 10:04:50 +0600

    Langchain added

--- server/server.js
+++ server/server.js
@@ -2,8 +2,14 @@ import 'dotenv/config';
 
 import express from 'express';
 
+import { OpenAI } from "langchain/llms/openai";
+
 const app = express();
 const PORT = process.env.PORT || 8001
+const llm = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
 app.get('/',(req,res)=>{
     res.send("hello world");
 });
