fbfab2fb4743c77543595825667dd68d3696d0f9
munaimun <munaimunz@gmail.com>
2024-03-03 15:43:01 +0600

    styled the form

--- client/src/Components/LoginPage.jsx
+++ client/src/Components/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css'
 
 const API_URL = "http://localhost:5001/api/v1";
 
@@ -36,36 +37,45 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <div className="inputs">
-          <label>Email:</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Email"
-            required
-            value={data.email}
-            onChange={(e) => setData({ ...data, email: e.target.value })}
-          />
-        </div>
+    <div className="flex justify-center items-center h-screen">
+        <form className='form flex justify-center flex-col' onSubmit={loginUser}>
 
-        <div className="inputs">
-          <label>Password:</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            required
-            value={data.password}
-            onChange={(e) => setData({ ...data, password: e.target.value })}
-          />
-        </div>
-        <button className="bg-orange-300 w-28 btn" type="submit">
-          Submit
-        </button>
-      </form>
+
+          <div className='form-contents'>
+            <div className="inputs">
+                <label>Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    required
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                />
+            </div>
+
+            <div className="inputs">
+                <label>Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    required
+                    value={data.password}
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                />
+            </div>
+          </div>
+
+            {/* <!-- BUTTON --> */}
+            <div>
+                <button className="bg-orange-300 w-28 btn place-items-center" type="submit">
+                    Submit
+                </button>
+            </div>
+        </form>
     </div>
+
   );
 };
 
