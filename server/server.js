35d10a8d48b7c36e00fa23d3fdbee5f702b1c601
dev-saiful <saifulcseian@gmail.com>
2024-03-05 15:33:30 +0600

    CI/CD Docker

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import cors from 'cors';
 import { errorhandler } from './middlewares/errorHandler.js';
 
 const app = express();
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 5001
 // middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
